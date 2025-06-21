7a708e162dd9c16ab556200fc28b84a4443283c4
Porawit Poboonma <ball6847@gmail.com>
2025-06-21 22:14:22 +0700

    refactor: remove unused FeatureFlags import from index.ts

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@
 import { Server } from "@modelcontextprotocol/sdk/server/index.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { SSEServerTransport } from "@modelcontextprotocol/sdk/server/sse.js";
-import { FeatureFlags, featureFlags, configPath, initFeatureFlags, hasLegacyFlags, setFeatureFlags } from './feature-flags.js';
+import { featureFlags, configPath, initFeatureFlags, hasLegacyFlags, setFeatureFlags } from './feature-flags.js';
 import { CallToolRequestSchema, ListToolsRequestSchema } from "@modelcontextprotocol/sdk/types.js";
 import nodeFetch from "node-fetch";
 import fetchCookie from "fetch-cookie";
